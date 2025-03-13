53c828b6f7d8517aa2b2470d8c50a81563f60ae7
Noann Ozier <n.ozier@lyceeclaudenougaro82300.onmicrosoft.com>
2025-03-13 11:16:15 +0100

    Changement ID pour l'hyperlien [categories.php]

--- categories.php
+++ categories.php
@@ -9,23 +9,12 @@ $categories = $db->query('SELECT * FROM categorie');
 <h1>Nos Produits</h1>
 <a href="addcategories.php">Ajouter une catégorie</a>
 <div class="categories">
-    <?php $i = 1; ?>
     <?php foreach ($categories as $category) : ?>
         <div class="container">
             <img src="<?= $category['IMAGE'] ?>" alt="">
-            <a href="produits.php?id=<?= $i ?>"><?= $category['LIBELLE'] ?></a>
+            <a href="produits.php?id=<?= $category['ID'] ?>"><?= $category['LIBELLE'] ?></a>
         </div>
-        <?php $i++; ?>
     <?php endforeach; ?>
-    <!-- <div class="container">
-        <img src="https://picsum.photos/64/64" alt="">
-        <a href="produits.php">Ordinateur Fixe & Portable</a>
-    </div>
-
-    <div class="container">
-        <img src="https://picsum.photos/64/64" alt="">
-        <a href="produits.php">Carte graphiques</a>
-    </div>-->
 </div>
 
 <?php
