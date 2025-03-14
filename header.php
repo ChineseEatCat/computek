fb115c0560b01974e909280f6e59a46daaa15e68
Noann Ozier <n.ozier@lyceeclaudenougaro82300.onmicrosoft.com>
2025-03-14 10:25:45 +0100

    Ajout de la bibliothèque SweetAlert2 dans le fichier header.php

--- header.php
+++ header.php
@@ -10,6 +10,7 @@
 	<link rel="stylesheet" href="css/footer.css">
 	<link rel="stylesheet" href="css/form.css">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+	<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
 
 <body>
