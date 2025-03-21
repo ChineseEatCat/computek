6aad35534fb6ab19330e6db33833090731a62ff1
HiroOkami <celianperrat512@gmail.com>
2025-03-21 15:10:16 +0100

    Utilisateur et role

--- sub.php
+++ sub.php
@@ -7,16 +7,24 @@ include 'config/bdd.php';
 var_dump($_POST);
 
 if (isset($_POST['email'])) {
-    $stmt = $db->query('SELECT * FROM utilisateurs WHERE EMAIL = "' . $_POST['email'] . '"');
+    $sql = 'SELECT * FROM utilisateurs WHERE EMAIL = :email';
+    $stmt = $db->prepare($sql);
+    $stmt->execute([':email' => $_POST['email']]);
     $resultat = $stmt->fetch();
 }
-
-if (empty($_POST))
 
 if ($_POST['pass'] == $_POST['repass']) {
     echo 'ok';
 } else {
     echo 'non';
+}
+
+if ($_POST['pass'] == $_POST['repass'] && $resultat == false) {
+    $sql = 'INSERT INTO utilisateurs (NOM, PRENOM, EMAIL, PASSWORD) VALUES (:nom, :prenom, :email, :pass)';
+    $stmt = $db->prepare($sql);
+    $stmt->execute([':nom' => $_POST['nom'], ':prenom' => $_POST['prenom'], ':email' => $_POST['email'], ':pass' => $_POST['pass']]);
+    $_SESSION['user'] = ['utilisateur' => $_POST['email'], 'password' => $_POST['pass'], 'role' => ($resultat['ADMIN'] == 1 ? 'admin' : 'user')];
+    header('Location: user.php');
 }
 
 ?>
