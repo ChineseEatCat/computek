0e389014dad30dfddff3f57e8def3292404970eb
HiroOkami <celianperrat512@gmail.com>
2025-03-27 12:01:00 +0100

    Utilisateur

---
+++ modifpassword.php
@@ -0,0 +1,56 @@
+<?php
+
+include 'header.php';
+
+include 'testuser.php';
+
+include 'config/bdd.php';
+
+$sql = 'SELECT * FROM utilisateurs WHERE EMAIL = :email AND PASSWORD = :password';
+$stmt = $db->prepare($sql);
+$stmt->execute([':email' => $_SESSION['user']['utilisateur'], ':password' => $_SESSION['user']['password']]);
+$result = $stmt->fetch();
+
+
+if ($result['PASSWORD'] == $_POST['oldpass'] && $_POST['newpass'] == $_POST['confirmation'] && !empty($_POST['newpass'])) {
+    $sql = 'UPDATE utilisateurs SET PASSWORD = :password WHERE EMAIL = :email';
+    $stmt = $db->prepare($sql);
+    $stmt->execute([':password' => $_POST['newpass'], ':email' => $_SESSION['user']['utilisateur']]);
+    $_SESSION['user']['password'] = $_POST['newpass'];
+    echo "<script>Swal.fire({
+        icon: 'success',
+        title: 'Modification du mot de passe',
+        text: 'Le mot de passe a été modifier avec succès.',
+    }).then(function() {
+        window.location.href = 'user.php';
+    });</script>";
+}
+
+?>
+
+<?php
+if ($result['PASSWORD'] != $_POST['oldpass'] || $_POST['newpass'] != $_POST['confirmation'] || empty($_POST['newpass'])) {
+?>
+<div class="user-box">
+    <h4>Modification du mot de passe</h4>
+    <?php
+    if ($result['PASSWORD'] != $_POST['oldpass'] || $_POST['newpass'] != $_POST['confirmation'] || empty($_POST['newpass'])) {
+        echo "<p class='msg-error'>*Mot de passe ou nouveau mot de passe incorrect*</p>";
+    }
+    ?>
+    <hr>
+    <div class="info-user">
+        <form class="modif-password" method="post" action="modifpassword.php">
+            <label for="mdp">Ancien mot de pass :</label>
+            <input type="password" id="mdp" name="oldpass">
+            <label for="mdp">Nouveau mot de passe :</label>
+            <input type="password" id="mdp" name="newpass">
+            <label for="mdp">Comfirmation :</label>
+            <input type="password" id="mdp" name="confirmation">
+            <input type="submit" value="Modifier">
+        </form>
+    </div>
+</div>
+<?php
+}
+?>
--- user.php
+++ user.php
@@ -2,6 +2,10 @@
 
 include 'header.php';
 
+include 'testuser.php';
+
+include 'config/bdd.php';
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['deconnexion']) && $_POST['deconnexion'] === 'disconnect') {
     // Se deconnecter
     $_SESSION['user'] = [];
@@ -9,22 +13,49 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['deconnexion']) && $_P
     header('Location: connexion.php');
 }
 
+$sql = 'SELECT * FROM utilisateurs WHERE EMAIL = :email AND PASSWORD = :password';
+$stmt = $db->prepare($sql);
+$stmt->execute([':email' => $_SESSION['user']['utilisateur'], ':password' => $_SESSION['user']['password']]);
+$resultat = $stmt->fetch();
+
 ?>
 
 <main>
     <h1>Utilisateur</h1>
     <div class="user-box">
-        <div class="menu-user">
-            <ul>
-                <li></li>
-                <li></li>
-                <li></li>
-            </ul>
+        <h4>INFORMATION</h4>
+        <hr>
+        <div class="info-user">
+            <div class="nom-prenom-user">
+                <p>Nom : <?= $resultat['NOM'] ?></p>
+                <p>Prénom : <?= $resultat['PRENOM'] ?></p>
+                <p>Rôle : <?= ($resultat['ADMIN'] == 1 ? 'Administrateur' : 'Utilisateur') ?></p>
+            </div>
+            <p>Email : <?= $resultat['EMAIL'] ?></p>
         </div>
-        <form action="user.php" method="post" style="margin-top: 20px;">
-            <input type="hidden" name="deconnexion" value="disconnect">
-            <button type="submit">Déconnexion</button>
-        </form>
+        <h4>Modification du mot de passe</h4>
+        <hr>
+        <div class="info-user">
+            <form class="modif-password" method="post" action="modifpassword.php">
+                <label for="mdp">Ancien mot de pass :</label>
+                <input type="password" id="mdp" name="oldpass">
+                <label for="mdp">Nouveau mot de passe :</label>
+                <input type="password" id="mdp" name="newpass">
+                <label for="mdp">Comfirmation :</label>
+                <input type="password" id="mdp" name="confirmation">
+                <input type="submit" value="Modifier">
+            </form>
+        </div>
+        <div class="button-user">
+            <form action="connexion.php" method="post">
+                <input type="hidden" name="deconnexion" value="disconnect">
+                <button type="submit" class="disconnect">Déconnexion</button>
+            </form>
+            <form action="suppruser.php" method="post">
+                <input type="hidden" name="suppruser" value="supprimer">
+                <button type="submit" class="suppr-user">Supprimer le compte</button>
+            </form>
+        </div>
     </div>
 </main>
 
