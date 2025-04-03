f85c55581e30d4ee0499d9e9a137df0478d24558
HiroOkami <celianperrat512@gmail.com>
2025-04-03 11:52:12 +0200

    Modification utilisateurs

---
+++ edituser.php
@@ -0,0 +1,75 @@
+<?php
+
+include 'header.php';
+
+include 'testadmin.php';
+
+$id = $_GET['id'] ?? null;
+
+if (isset($_POST) && !empty($_POST) && $_POST['pass'] == $_POST['repass']) {
+
+    var_dump($_POST);
+    $sql = 'UPDATE utilisateurs SET NOM = ?, PRENOM = ?, EMAIL = ?, PASSWORD = ?, ADMIN = ? WHERE ID = ?';
+    $stmt = $db->prepare($sql);
+    $stmt->execute([$_POST['nom'], $_POST['prenom'], $_POST['email'], $_POST['pass'], $_POST['role'], $id]);
+
+    header('Location: panel.php');
+}
+
+if (!empty($_GET['id'])) {
+    $sql = 'SELECT * FROM utilisateurs WHERE ID = :id';
+    $stmt = $db->prepare($sql);
+    $stmt->execute([':id' => $_GET['id']]);
+    $infoUser = $stmt->fetch();
+
+    var_dump($infoUser);
+    ?>
+
+    <form class="edituser" method="post" action="#">
+		<h4>MODIFIER L'UTILISATEUR</h4>
+		<hr>
+		<div class="nom-prenom">
+			<div>
+				<label>Nom :</label>
+				<input type="text" name="nom" value="<?= $infoUser['NOM'] ?>">
+			</div>
+			<div>
+				<label>Prénom :</label>
+				<input type="text" name="prenom" value="<?= $infoUser['PRENOM'] ?>">
+			</div>
+		</div>
+		<label>E-mail :</label>
+		<input type="email" name="email" value="<?= $infoUser['EMAIL'] ?>">
+		<label>Mot de passe :</label>
+		<input type="password" name="pass" value="<?= $infoUser['PASSWORD'] ?>">
+		<label>Confirmation mot de passe :</label>
+		<input type="password" name="repass" value="<?= $infoUser['PASSWORD'] ?>">
+        <label for="role">Rôle :</label>
+        <select name="role" class="choixRole">
+            <option value="1" <?= $infoUser['ADMIN'] == '1' ? 'selected' : '' ?>>Admin</option>
+            <option value="0" <?= $infoUser['ADMIN'] == '0' ? 'selected' : '' ?>>Utilisateur</option>
+        </select>
+		<input type="submit" value="Modifier">
+	</form>
+
+    <?php
+}
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+
+include 'footer.php';
+
+?>
